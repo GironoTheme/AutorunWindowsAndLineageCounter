00606d6300e5df7e4a736368f550a96131373025
LexaNeboxod <gironotheme@gmail.com>
2024-07-03 21:26:53 +0300

    fixed sequence

--- launch_script.py
+++ launch_script.py
@@ -2,9 +2,9 @@ from Purple.autorun_lineage_windows import autorun_lineage_windows
 from Purple.purple import PurpleSingleton
 from LineageCounter.launch_search import launch_search
 
-launch_search()
 purple_instance = PurpleSingleton.get_instance()
 purple_instance.launch_purple()
 
 autorun_lineage_windows.launch()
+launch_search()
 
