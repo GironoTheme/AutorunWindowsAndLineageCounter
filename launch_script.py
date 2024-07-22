140c37ceeec5cf80ad474aa05d11cdad2f5258ff
LexaNeboxod <gironotheme@gmail.com>
2024-07-22 15:01:17 +0300

    added delay

--- launch_script.py
+++ launch_script.py
@@ -2,8 +2,8 @@ from Purple.autorun_la_windows import autorun_lineage_windows
 from LA.go_to_world import move_and_click, sleep
 from Counter.launch_search import launch_search
 
-# sleep(6)
-# move_and_click(888, 466)
+sleep(6)
+move_and_click(888, 466)
 
 autorun_lineage_windows.launch()
 launch_search()
